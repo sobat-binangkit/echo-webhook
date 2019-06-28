8d74395b2e43b6012087932f3a5e78a7b1057afd
Ahmad R. Djarkasih <sobat.binangkit@gmail.com>
2019-06-28 22:21:02 +0700

    Use seperated handlers

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo"
+
+	"github.com/sobat-binangkit/webhook/handlers"
 )
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+
+	e.GET("/", handlers.HelloWorldHandler)
+
 	e.Logger.Fatal(e.Start(":8080"))
 }
