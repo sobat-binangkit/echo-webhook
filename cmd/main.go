7eb0b0ea099bdddce88258cbd56ac15a0dd61d9b
Ahmad R. Djarkasih <djarkasih@gmail.com>
2019-06-29 21:06:03 +0700

    Use TLS if domain name set in environment variable

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/labstack/echo"
 	"github.com/sobat-binangkit/webhook/handlers"
+
+	"golang.org/x/crypto/acme/autocert"
 )
 
 func main() {
@@ -16,9 +18,21 @@ func main() {
 		path = "/"
 	}
 
-	fmt.Printf("Setting handler for %s", path)
+	domain := os.Getenv("DOMAIN_NAME")
+
+	fmt.Printf("Domain name = %s\n", domain)
+	fmt.Printf("Setting handler for %s\n", path)
 
 	e.GET(path, handlers.HelloWorldHandler)
 
-	e.Logger.Fatal(e.Start(":8080"))
+	if domain == "" {
+		e.Logger.Fatal(e.Start(":8080"))
+	} else {
+		e.AutoTLSManager.HostPolicy = autocert.HostWhitelist("webhook.id")
+		// Cache certificates
+		e.AutoTLSManager.Cache = autocert.DirCache(".")
+		e.Logger.Fatal(e.StartAutoTLS(":443"))
+
+	}
+
 }
